8e288882273661c93b9c2aed1e150c9be6811462
Inchijeong <cde0409@naver.com>
2018-05-05 02:16:24 +0900

    Start Typescript tutorial

---
+++ data-type.js
@@ -0,0 +1,5 @@
+var myVar2 = true;
+var myVar = false;
+var myBooleanVar = new Boolean(true);
+console.log(myVar.valueOf());
+console.log(myBooleanVar.valueOf());
---
+++ data-type.ts
@@ -0,0 +1,9 @@
+let myVar2: boolean = true;
+let myVar: boolean = false;
+
+let myBooleanVar: Boolean = new Boolean(true);
+
+console.log(myVar.valueOf());
+console.log(myBooleanVar.valueOf());
+
+
